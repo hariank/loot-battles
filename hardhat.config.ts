b31e4ee6f7f32605c8ac920f06a96a7566a14fc2
Hariank Muthakana <hariank3k@gmail.com>
2021-09-04 23:41:29 -0400

    update deploy workflow

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,24 +7,32 @@ dotenv.config();
 
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
+import * as ethers from "ethers";
 import "hardhat-abi-exporter";
 import { task } from "hardhat/config";
-import { ethers } from "hardhat";
 import { HardhatUserConfig } from "hardhat/config";
+
+task("balance", "Prints an account's balance")
+  .addParam("address", "The account's address")
+  .setAction(async (taskArgs, hre) => {
+    const provider = hre.ethers.provider;
+    const balance = await provider.getBalance(taskArgs.address);
+    console.log(ethers.utils.formatEther(balance).toString(), "eth");
+  });
 
 export function forknet() {
   return {
     url: process.env.RPC_URL_MAINNET,
-    blockNumber: 13135486,
+    blockNumber: 13163246,
   };
 }
 
 const config: HardhatUserConfig = {
   solidity: "0.8.0",
   networks: {
-    hardhat: {
-      forking: forknet(),
-    },
+    // hardhat: {
+    //   forking: forknet(),
+    // },
     testnet: {
       url: process.env.RPC_URL_TESTNET,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -7,21 +7,67 @@ import { BigNumber, Contract, ContractFactory } from "ethers";
 import * as utils from "./utils";
 import * as constants from "./constants";
 
-async function main() {
-  const [deployer] = await ethers.getSigners();
-
+async function logDeployerInfo(deployer) {
   let cp: BigNumber = await deployer.getGasPrice();
   console.log("Current gas price: ", cp.toString());
   console.log("Deployer:", deployer.address);
   console.log("Deployer balance:", (await deployer.getBalance()).toString());
+}
 
-  const c: Contract = await utils.deployContractFromCompiled(
+async function deployPeripheralContracts() {
+  const loot: Contract = await utils.deployContractFromCompiled("Loot");
+  const lootAddr = loot.address;
+  console.log("Loot contract deployed to:", lootAddr);
+
+  const lootComp: Contract = await utils.deployContractFromCompiled(
+    "LootComponents"
+  );
+  const lootCompAddr = lootComp.address;
+  console.log("LootComponents contract deployed to:", lootCompAddr);
+
+  const agld: Contract = await utils.deployContractFromCompiled(
+    "AdventureGold",
+    lootAddr
+  );
+  const agldAddr = agld.address;
+  console.log("AdventureGold contract deployed to:", agldAddr);
+
+  return [lootAddr, lootCompAddr, agldAddr];
+}
+
+async function deployTestnet() {
+  const [deployer] = await ethers.getSigners();
+
+  await logDeployerInfo(deployer);
+
+  const [lootAddr, lootCompAddr, agldAddr] = await deployPeripheralContracts();
+
+  const battler: Contract = await utils.deployContractFromCompiled(
+    "LootBattler",
+    lootAddr,
+    lootCompAddr,
+    agldAddr
+  );
+  console.log("LootBattler contract deployed to:", battler.address);
+}
+
+async function deployMainnet() {
+  const [deployer] = await ethers.getSigners();
+
+  await logDeployerInfo(deployer);
+
+  const battler: Contract = await utils.deployContractFromCompiled(
     "LootBattler",
     constants.LOOT_MAIN_ADDR,
     constants.LOOT_COMPONENTS_ADDR,
     constants.AGLD_ADDR
   );
-  console.log("Contract deployed to:", c.address);
+  console.log("LootBattler contract deployed to:", battler.address);
+}
+
+async function main() {
+  await deployTestnet();
+  // await deployMainnet();
 }
 
 main()
